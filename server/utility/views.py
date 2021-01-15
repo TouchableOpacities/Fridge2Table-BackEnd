d6e0d33b5ad8650ba97bf3e0b25c93a85a86338a
Jackson Golden <jackson.golden@gmail.com>
2021-01-15 18:21:20 -0500

    Restructured - views is now the main script

--- server/utility/views.py
+++ server/utility/views.py
@@ -1,28 +1,26 @@
 from django.http import JsonResponse
-import json
-from .scripts import recipe_puppy_api
+from .scripts import utils
+from .scripts import recipe_puppy
+
 
 
 # Uses RecipePuppy API to get list of recipes with given ingredients.
 # Routed from urlpattern "getRecipesFromIngredients/" in urls.py.
 def get_recipes(request):
 
-    # Get data from request (a list of strings = ingredients)
-    data_in = json.loads(request.body)          # the body of the request is a list of ingredients (strings)
-    ingredients = data_in['ingredients']
-    dish_type_str = ""                          # placeholder for now
+    # Receive request from app, get list of ingredients (list of strings)
+    ingredients = utils.process_incoming_data(request)
 
-
-    # Log data received to console
-    print("POST INFO: Ingredients = " + ingredients)
-    print("(Dish type blank for now...)")
-
-    # Call recipe_puppy_api.get_recipes with specified ingredients.
+    # Query RecipePuppy for specified list of ingredients
     # Returns list of dicts of recipes that look like:
     # [{'href': 'http://allrecipes.com/Recipe/Ds-Famous-Salsa/Detail.aspx',
     #   'ingredients': 'green chilies, garlic, lime, onions, salt',
     #   'thumbnail': 'http://img.recipepuppy.com/29584.jpg',
     #   'title': "D's Famous Salsa"}, ...]
-    recipes = recipe_puppy_api.get_recipes(ingredients, dish_type_str)
+    query_result = recipe_puppy.get_recipes(ingredients, "")        # empty string is placeholder for, e.g., "omelet"
 
-    return JsonResponse(recipes, safe=False)
+    # Process query results. Result is final list of dictionaries for returning to frontend.
+    final_result = utils.process_query_result(query_result)
+
+    return JsonResponse(final_result, safe=False)
+
