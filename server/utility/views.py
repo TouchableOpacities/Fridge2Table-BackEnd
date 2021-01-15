0e13e7bac231c40430a17219eb1d76373624022e
Jackson Golden <jackson.golden@gmail.com>
2021-01-15 17:16:11 -0500

    Added some comments and removed old code

--- server/utility/views.py
+++ server/utility/views.py
@@ -3,23 +3,26 @@ import json
 from .scripts import recipe_puppy_api
 
 
+# Uses RecipePuppy API to get list of recipes with given ingredients.
+# Routed from urlpattern "getRecipesFromIngredients/" in urls.py.
 def get_recipes(request):
 
-    # get data from request
-    dataIn = json.loads(request.body)  # {'name':'John', 'age': 42}
-    print("Post Info:")
-    print(dataIn['ingredients'])
-    ingredients = dataIn['ingredients']
-    dish_type_str = ""
+    # Get data from request (a list of strings = ingredients)
+    data_in = json.loads(request.body)          # the body of the request is a list of ingredients (strings)
+    ingredients = data_in['ingredients']
+    dish_type_str = ""                          # placeholder for now
 
-    # make call to getRecipes function
+
+    # Log data received to console
+    print("POST INFO: Ingredients = " + ingredients)
+    print("(Dish type blank for now...)")
+
+    # Call recipe_puppy_api.get_recipes with specified ingredients.
+    # Returns list of dicts of recipes that look like:
+    # [{'href': 'http://allrecipes.com/Recipe/Ds-Famous-Salsa/Detail.aspx',
+    #   'ingredients': 'green chilies, garlic, lime, onions, salt',
+    #   'thumbnail': 'http://img.recipepuppy.com/29584.jpg',
+    #   'title': "D's Famous Salsa"}, ...]
     recipes = recipe_puppy_api.get_recipes(ingredients, dish_type_str)
 
-    # dataOut= {
-    #     'id': 1234,
-    #     'name': 'Calvin',
-    #     'age': 6,
-    #     'known associates': 'Hobbes, Suzie'
-    #
-    # }
     return JsonResponse(recipes, safe=False)
